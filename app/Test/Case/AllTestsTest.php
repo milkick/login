0fbbd02783ec60e48581f7db3ac5b2e0e5945818
Windows7 <Windows7@Windows10-PC>
2016-11-23 22:20:41 +0900

    PHPUnit関連テスト
    Alltest.phpを追加

---
+++ app/Test/Case/AllTestsTest.php
@@ -0,0 +1,11 @@
+<?php
+
+class AllTests extends CakeTestSuite {
+    public static function suite() {
+        $suite = new CakeTestSuite('All tests');
+        $suite->addTestDirectory(TESTS . 'Case' . DS . 'Controller');
+        $suite->addTestDirectory(TESTS . 'Case' . DS . 'Model');
+        $suite->addTestDirectory(TESTS . 'Case' . DS . 'View');
+        return $suite;
+    }
+}
